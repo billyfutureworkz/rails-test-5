df69180c5185707e883828b337d84b16b58cfddd
billy <billy@futureworkz.com>
2015-12-19 09:51:07 +0700

    added spec for enumerize

--- spec/models/school_spec.rb
+++ spec/models/school_spec.rb
@@ -4,5 +4,6 @@ RSpec.describe School, type: :model do
   context 'validation' do 
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:address) }
+    it { should enumerize(:type).in(:university, :high_school, :secondary_school, :primary_school) }
   end
 end

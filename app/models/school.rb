78ceb381a963bfc44a4d02dcc9d38bdd768d1ced
billy <billy@futureworkz.com>
2015-12-19 09:59:42 +0700

    added associations has_many to school

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,7 +1,9 @@
 class School < ActiveRecord::Base
   extend Enumerize
   enumerize :type, in: [:university, :high_school, :secondary_school, :primary_school]
-  
+
   validates :name, presence: true
   validates :address, presence: true
+
+  has_many :users
 end

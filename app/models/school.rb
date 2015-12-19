ae96ec5ada66bcc76799a798123b617e92824938
billy <billy@futureworkz.com>
2015-12-19 09:50:50 +0700

    added enumerize type in school

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,4 +1,7 @@
 class School < ActiveRecord::Base
+  extend Enumerize
+  enumerize :type, in: [:university, :high_school, :secondary_school, :primary_school]
+  
   validates :name, presence: true
   validates :address, presence: true
 end

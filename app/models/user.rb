a8ed7fe7c0c3b6c958aaf26c5c723a8ed2079c46
billy <billy@futureworkz.com>
2015-12-19 10:13:29 +0700

    added spec year of experience

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   validates :name, presence: true
   validates :year_of_experience, presence: true
+  validates :year_of_experience, numericality: { greater_than_or_equal_to: 0 }
 
   belongs_to :school
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe User, type: :model do
   context 'validation' do 
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:year_of_experience) }
+    it { should validate_numericality_of(:year_of_experience).is_greater_than_or_equal_to(0) }
   end
 
   context 'association' do 

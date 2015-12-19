3a6f7aa43b98b5a940697cdbd204cf8ef2846019
billy <billy@futureworkz.com>
2015-12-19 11:03:57 +0700

    added spec for office_worker and student

--- spec/models/office_worker_spec.rb
+++ spec/models/office_worker_spec.rb
@@ -7,4 +7,17 @@ RSpec.describe OfficeWorker, type: :model do
     it { should validate_presence_of(:year_of_experience) }
     it { should validate_numericality_of(:year_of_experience).is_greater_than_or_equal_to(0) }
   end
+
+  context 'custom validation' do
+    describe 'at_least_20' do 
+      before do
+        @user = User.new(name: 'student', dob: '19-12-1999', type: 'OfficeWorker')
+      end
+
+      it "should not be valid if dob if less than 18 year olds" do
+        @user.valid?
+        expect(@user.errors.messages[:dob]).to include('You must be 20 years or older.')
+      end
+    end
+  end
 end
--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -3,6 +3,24 @@ require 'rails_helper'
 RSpec.describe Student, type: :model do
   it_behaves_like "an STI class"
 
-  context 'validation' do 
+  context 'custom validation' do
+    describe 'at_least_18' do 
+      before do
+        @user = User.new(name: 'student', dob: '19-12-1999', type: 'Student')
+      end
+
+      it "should not be valid if dob if less than 18 year olds" do
+        @user.valid?
+        expect(@user.errors.messages[:dob]).to include('You must be 18 years or older.')
+      end
+    end
   end
 end
+
+
+
+   
+
+    # it "raises an error if end time is lower than start time" do
+    #   @time_event.errors.should include("An event can not be finished if it did not start yet...")
+    # end

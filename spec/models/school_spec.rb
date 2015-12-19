3208893200c67dae3b176ea8900fda76eb4326e8
billy <billy@futureworkz.com>
2015-12-19 10:03:18 +0700

    added spec associations between school and user

--- spec/models/school_spec.rb
+++ spec/models/school_spec.rb
@@ -6,4 +6,8 @@ RSpec.describe School, type: :model do
     it { should validate_presence_of(:address) }
     it { should enumerize(:type).in(:university, :high_school, :secondary_school, :primary_school) }
   end
+
+  context 'association' do 
+    it { should have_many(:users) }
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  context 'association' do 
+    it { should belong_to(:school) }
+  end
 end

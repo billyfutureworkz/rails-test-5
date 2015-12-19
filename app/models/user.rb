2de4fb5d552fd4aeda457dfb54aebfed19556bb1
billy <billy@futureworkz.com>
2015-12-19 10:07:09 +0700

    added validations and spec for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
+  validates :name, presence: true
+  validates :year_of_experience, presence: true
+
   belongs_to :school
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  context 'validation' do 
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:year_of_experience) }
+  end
+
   context 'association' do 
     it { should belong_to(:school) }
   end
